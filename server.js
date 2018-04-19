b502f0b4c3ab38052a564abddbaccdec32312166
fcarlone <frankcarlone@gmail.com>
2018-04-19 15:01:26 -0400

    correct port variable

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
-const port = process.env.Port || 3000;
+const port = listen(process.env.Port || 3000);
 
 var app = express();
 
